b4a28c37b1d22fe055775e60970358c6ec28810c
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2025-05-18 18:59:16 +0700

    add PlacementDepositRepository

---
+++ src/main/java/com/bayu/thread_spring_boot/repository/PlacementDepositRepository.java
@@ -0,0 +1,8 @@
+package com.bayu.thread_spring_boot.repository;
+
+import com.bayu.thread_spring_boot.model.PlacementDeposit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlacementDepositRepository extends JpaRepository<PlacementDeposit, String> {
+
+}
