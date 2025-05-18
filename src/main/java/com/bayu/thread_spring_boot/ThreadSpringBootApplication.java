0ae16201bbbc5a4a3ac6227b073108232814b1f1
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2025-05-18 10:37:43 +0700

    init

---
+++ src/main/java/com/bayu/thread_spring_boot/ThreadSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.thread_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/thread_spring_boot/ThreadSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.thread_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
